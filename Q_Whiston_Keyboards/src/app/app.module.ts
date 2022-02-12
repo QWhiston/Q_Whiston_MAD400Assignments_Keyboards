1dab49b756888b25ab653e311badddfec652ceb0
QWhiston <w0762119@myscc.ca>
2022-02-12 15:13:48 -0500

    Pipe added

--- Q_Whiston_Keyboards/src/app/app.module.ts
+++ Q_Whiston_Keyboards/src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+<<<<<<< Updated upstream
+=======
+import { ContentListComponent } from './content-list/content-list.component';
+import { TypeSetPipe } from './type-set.pipe';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     ContentCardComponent
+=======
+    ContentCardComponent,
+    ContentListComponent,
+    TypeSetPipe
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule
--- Q_Whiston_Keyboards/src/app/content-card/content-card.component.ts
+++ Q_Whiston_Keyboards/src/app/content-card/content-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
@@ -54,7 +55,18 @@ export class ContentCardComponent implements OnInit {
 
   }
 
+  @Input() 
+  content?: Content;
+
+  constructor() {
+
+  }
+
   ngOnInit(): void {
+  }
+
+  logId(): void{
+    console.log("id: ", this.content?.id, " | title is: ", this.content?.title);
   }
 
 }
